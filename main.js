0b08f9f974fa877991a57bc3d0b8c20424609306
Mikaeru69 <31444539+Mikaeru69@users.noreply.github.com>
2018-06-01 19:22:33 +0200

    Moved out app-related event handlers

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow, globalShortcut, ipcMain } = require ('electron');
-app.dock.hide ();
 let mainWindow = null;
 function onAppReady ()
 {
@@ -8,16 +7,16 @@ function onAppReady ()
         {
             width: 600,
             height: 600,
-            show: true,
+            show: false,
             frame: false
         }
     );
     mainWindow.loadURL (`file://${__dirname}/index.html`);
     mainWindow.once ('closed', () => { mainWindow = null; });
     mainWindow.on ('blur', () => { mainWindow.hide (); });
-    app.once ('window-all-closed', () => { app.quit (); });
-//    app.on ('activate', () => { mainWindow.show (); });
     globalShortcut.register ("CommandOrControl+Alt+P", () => { mainWindow.show (); });
     ipcMain.on ('dismiss', () => { app.hide (); });
 }
 app.once ('ready', onAppReady);
+app.once ('window-all-closed', () => { app.quit (); });
+app.dock.hide ();
