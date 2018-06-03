83815cc2dba972daa4761f9802879392cb4adcda
Mikaeru69 <31444539+Mikaeru69@users.noreply.github.com>
2018-06-03 17:28:41 +0200

    Add files via upload

--- renderer.js
+++ renderer.js
@@ -1,13 +1,13 @@
 const { ipcRenderer } = require ('electron');
 document.addEventListener
 (
-	'keydown',
-	(event) =>
-	{
-		if (event.key === 'Enter')
-		{
-			event.preventDefault ();
-			ipcRenderer.send ('dismiss');
-		}
-	}
+    'keydown',
+    (event) =>
+    {
+        if (event.key === 'Enter')
+        {
+            event.preventDefault ();
+            ipcRenderer.send ('dismiss');
+        }
+    }
 );
